88e3d4697f6ccdbdc5e40d18027a5eae2ac8bb22
pengchenggang <76526211@qq.com>
2024-11-12 15:18:51 +0800

    up

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/form-generator-iview/' : '/'
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: BASE_URL,
   outputDir: 'docs'
 })
